72578614a8c6f705837b911bf3b12043f59081e4
olecsiuyae <olecsiuyae@gmail.com>
2017-05-11 13:11:50 +0300

    Add 2 implementations: JsStyle and CSharpStyle

---
+++ CSharp/SimpleEventFlow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowProgrammingWorks.CSharpEvents
+{
+    public class SimpleEventFlow
+    {
+        private event EventHandler SimpleEvent;
+
+        public void Subscribe()
+        {
+            SimpleEvent += DoWork;
+        }
+
+        public void Unsubscribe()
+        {
+            SimpleEvent -= DoWork;
+        }
+
+        public void Invoke()
+        {
+            SimpleEvent.Invoke(this,new EventArgs());
+        }
+
+        private void DoWork(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Event invoked by sender {sender}. Event arguments - {e}");
+        }
+    }
+}
