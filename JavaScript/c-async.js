f8c0ea1834d71bd616abb844b6cfcd3d2f38ab0a
tshemsedinov <timur.shemsedinov@gmail.com>
2019-06-02 21:25:12 +0300

    Implement AsyncEventEmitter

---
+++ JavaScript/c-async.js
@@ -0,0 +1,96 @@
+'use strict';
+
+class AsyncEventEmitter {
+  constructor() {
+    this.events = new Map();
+    this.wrappers = new Map();
+  }
+
+  on(name, fn) {
+    const event = this.events.get(name);
+    if (event) event.add(fn);
+    else this.events.set(name, new Set([fn]));
+  }
+
+  once(name, fn) {
+    const wrapper = (...args) => {
+      this.remove(name, wrapper);
+      fn(...args);
+    };
+    this.wrappers.set(fn, wrapper);
+    this.on(name, wrapper);
+  }
+
+  async emit(name, ...args) {
+    const event = this.events.get(name);
+    if (!event) return;
+    const promises = [...event.values()].map(fn => fn(...args));
+    return Promise.all(promises);
+  }
+
+  remove(name, fn) {
+    const event = this.events.get(name);
+    if (!event) return;
+    if (event.has(fn)) {
+      event.delete(fn);
+      return;
+    }
+    const wrapper = this.wrappers.get(fn);
+    if (wrapper) {
+      event.delete(wrapper);
+      if (event.size === 0) this.events.delete(name);
+    }
+  }
+
+  clear(name) {
+    if (name) this.events.delete(name);
+    else this.events.clear();
+  }
+
+  count(name) {
+    const event = this.events.get(name);
+    return event ? event.size : 0;
+  }
+
+  listeners(name) {
+    const event = this.events.get(name);
+    return new Set(event);
+  }
+
+  names() {
+    return [...this.events.keys()];
+  }
+}
+
+// Usage
+
+const ee = new AsyncEventEmitter();
+
+(async () => {
+
+  ee.on('e1', async () => {
+    console.log('e1 listener 1');
+  });
+
+  ee.on('e1', async () => {
+    console.log('e1 listener 2');
+  });
+
+  ee.on('e1', async () => {
+    console.log('e1 listener 3');
+  });
+
+  ee.on('e2', async () => {
+    console.log('e2 listener 1');
+  });
+
+  ee.on('e2', async () => {
+    console.log('e2 listener 2');
+  });
+
+  console.log('begin');
+  await ee.emit('e1');
+  await ee.emit('e2');
+  console.log('end');
+
+})();
